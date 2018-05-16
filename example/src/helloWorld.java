60123bbedcacacefb159b64af9a6d51993417b8e
philipholleb <philipholleb@gmail.com>
2018-05-16 09:55:37 -0700

    typo fix

--- example/src/helloWorld.java
+++ example/src/helloWorld.java
@@ -1,6 +1,6 @@
 public class helloWorld {
     public static void main(String[]args){
-        System.out.println("Hello World comminted");
+        System.out.println("Hello World committed");
 
     }
 }
