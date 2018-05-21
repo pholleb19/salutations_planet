11d0cf9bf831cebb674f60b7b776bcfcb9b211af
philipholleb <philipholleb@gmail.com>
2018-05-21 08:27:02 -0700

    change test branch comment

--- example/src/helloWorld.java
+++ example/src/helloWorld.java
@@ -1,6 +1,7 @@
 public class helloWorld {
     public static void main(String[]args){
         System.out.println("Hello World committed");
-
+        System.out.println("next branch");
+        //COMMENT FOR TEST
     }
 }
